b19e0c1dc2f75c3d2e9ab4d707941374decebad4
Wadih Ojeil <wadih.ojeil@gmail.com>
2021-09-17 18:48:57 -0400

    removed the consol log since I can see the smart contract and adding assert.ok to check if the address exists

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,18 +5,20 @@ const ganache = require( 'ganache-cli');
 const Web3 = require("web3");
 //instance of web 3 below 
 const web3 = new Web3(ganache.provider());
+//import compiled solidity smart contract
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () =>{
 // get a list of all accounts
 accounts = await web3.eth.getAccounts();
 
-
-
-
 //Use one of the those accounts to deploy the contract
-
+inbox = await new web3.eth.Contract(JSON.parse(interface))
+ .deploy({ data: bytecode, arguments: ['Helloz']})
+ .send({from: accounts[0], gas: '1000000'})
 
 });
 
@@ -24,6 +26,6 @@ accounts = await web3.eth.getAccounts();
 //testing out the fetch account works 
 describe('Inbox', ()=>{
     it('deploys a contract', ()=>{
-console.log(accounts); 
+assert.ok(inbox.options.address);
     });
 });
